66144d7d6b1893913e76f9f074a078fff3b42746
Weiyanyu <chinayeonon@gmail.com>
2017-11-26 16:05:24 +0800

    初级排序算法

---
+++ src/ch2/section1/SelectSort.java
@@ -0,0 +1,57 @@
+package ch2.section1;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+
+public class SelectSort {
+
+
+    public static void sort(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            int min = i;
+            for (int j = i+1; j < a.length; j++) {
+                if (less(a[j], a[min]))
+                    min = j;
+            }
+            exch(a, i, min);
+        }
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void show(Comparable[] a) {
+        for (Comparable e : a) {
+            System.out.print(e + " ");
+        }
+        System.out.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i-1])) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] a = In.readInts();
+        Integer[] A = new Integer[a.length];
+        for (int i = 0; i < a.length; i++) {
+            A[i] = a[i];
+        }
+        SelectSort.sort(A);
+        assert SelectSort.isSorted(A);
+        SelectSort.show(A);
+    }
+
+
+}
