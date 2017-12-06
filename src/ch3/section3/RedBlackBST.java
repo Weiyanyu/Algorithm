cb1fa4540e75583d298c36a24b22074ead65d43f
Weiyanyu <chinayeonon@gmail.com>
2017-12-06 14:18:58 +0800

    红黑树的插入操作

---
+++ src/ch3/section3/RedBlackBST.java
@@ -0,0 +1,93 @@
+package ch3.section3;
+
+public class RedBlackBST<Key extends Comparable<Key>, Value> {
+
+    private static final boolean BLACK = false;
+    private static final boolean RED = true;
+
+    private Node root;
+
+    private class Node {
+        Key key;
+        Value val;
+        Node left;
+        Node right;
+        int N;
+        boolean color;
+
+        Node(Key key, Value val, int N, boolean color) {
+            this.key = key;
+            this.val = val;
+            this.N = N;
+            this.color = color;
+        }
+    }
+
+    private boolean isRed(Node x) {
+        if (x == null) return false;
+        return x.color == RED;
+    }
+
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) return 0;
+        else return x.N;
+    }
+
+    private Node rotateLeft(Node h) {
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+        x.color = h.color;
+        h.color = RED;
+        x.N = h.N;
+        h.N = size(h.left) + size(h.right) + 1;
+        return x;
+    }
+
+    private Node rotateRight(Node h) {
+        Node x = h.left;
+        h.left = x.right;
+        x.right = h;
+        x.color = h.color;
+        h.color = RED;
+        x.N = h.N;
+        h.N = size(h.left) + size(h.right) + 1;
+        return x;
+    }
+
+    public void put(Key key, Value val) {
+        root = put(root, key, val);
+        root.color = BLACK;
+    }
+
+
+    private void flipColors(Node h) {
+        h.color = RED;
+        h.left.color = BLACK;
+        h.right.color = BLACK;
+    }
+
+    private Node put(Node h, Key key, Value val) {
+        if (h == null) {
+            return new Node(key, val, 1, RED);
+        }
+        int comp = key.compareTo(h.key);
+        if (comp < 0) h.left = put(h.left, key, val);
+        else if (comp > 0) h.right = put(h.right, key, val);
+        else h.val = val;
+
+        if (isRed(h.right) && !isRed(h.left)) h = rotateLeft(h);
+        if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
+        if (isRed(h.left) && isRed(h.right)) flipColors(h);
+
+        h.N = size(h.left) + size(h.right) + 1;
+        return h;
+    }
+
+
+}
